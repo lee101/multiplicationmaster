bca823ee065ffd95b7c257f55913ddb47fa291af
Lee Penkman <leepenkman@gmail.com>
2014-06-24 16:37:57 +1200

    Cleanup

--- gameon/static/js/gameon.js
+++ gameon/static/js/gameon.js
@@ -973,6 +973,17 @@ Object.keys = Object.keys || function (o) {
     return result;
 };
 
+Array.prototype.shuffle = function () {
+    for (var i = this.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = this[i];
+        this[i] = this[j];
+        this[j] = tmp;
+    }
+
+    return this;
+};
+
 String.prototype.reverse = function () {
     return this.split("").reverse().join("");
 };
